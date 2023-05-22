e214853d16d65c795f906962105a96dc0b3a89d7
Keila Aparecida Rpdrogues Passos <keila.rodrigues.passos@gmail.com>
2023-05-22 17:34:15 -0300

    feat: adicionei console.logs para que os resultados de cada operação possam ser vistos

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from "./class/CompanyAccount";
+import { PeopleAccount } from "./class/PeopleAccount";
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, "Nath", 10);
+// console.log(peopleAccount);
+console.log(">>>>> >>>>> >>>>> peopleAccount");
+peopleAccount.deposit(20);
+peopleAccount.withdraw(2);
+peopleAccount.withdraw(47);
+
+const companyAccount: CompanyAccount = new CompanyAccount("DIO", 20);
+// console.log(companyAccount);
+console.log(">>>>> >>>>> >>>>> companyAccount");
+companyAccount.deposit(30);
+companyAccount.withdraw(4);
+companyAccount.withdraw(10);
+companyAccount.withdraw(50);
