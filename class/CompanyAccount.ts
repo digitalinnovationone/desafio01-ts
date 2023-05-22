bba781c1fa82f301a16f3af48a46afee43e54959
Keila Aparecida Rpdrogues Passos <keila.rodrigues.passos@gmail.com>
2023-05-22 18:12:00 -0300

    feat: implementei o método getLoan

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -1,12 +1,16 @@
-import { DioAccount } from "./DioAccount"
+import { DioAccount } from "./DioAccount";
 
 export class CompanyAccount extends DioAccount {
-
-  constructor(name: string, accountNumber: number){
-    super(name, accountNumber)
+  constructor(name: string, accountNumber: number) {
+    super(name, accountNumber);
   }
 
-  getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
-  }
+  getLoan = (value: number): void => {
+    if (this.getStatus()) {
+      this.balance += value;
+      console.log(`---------- EMPRÉSTIMO ----------`);
+      console.log(`Você pegou um empréstimo de R$ ${value}`);
+      console.log(`Saldo em conta atualizado: R$ ${this.balance}`);
+    }
+  };
 }
