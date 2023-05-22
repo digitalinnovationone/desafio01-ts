191065fdaca1b5a9f4da805cabbda1ff9df9430d
Keila Aparecida Rpdrogues Passos <keila.rodrigues.passos@gmail.com>
2023-05-22 17:32:51 -0300

    feat: implementei as operações de saque e depósito na classe DioAccount

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,42 +1,57 @@
 export abstract class DioAccount {
-  private name: string
-  private readonly accountNumber: number
-  balance: number = 0
-  private status: boolean = true
+  private name: string;
+  private readonly accountNumber: number;
+  balance: number = 0;
+  private status: boolean = true;
 
-  constructor(name: string, accountNumber: number){
-    this.name = name
-    this.accountNumber = accountNumber
+  constructor(name: string, accountNumber: number) {
+    this.name = name;
+    this.accountNumber = accountNumber;
   }
 
   setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
-  }
+    this.name = name;
+    console.log("Nome alterado com sucesso!");
+  };
 
   getName = (): string => {
-    return this.name
-  }
+    return this.name;
+  };
 
-  deposit = (): void => {
-    if(this.validateStatus()){
-      console.log('Voce depositou')
+  deposit = (value: number): void => {
+    if (this.validateStatus()) {
+      this.balance += value;
+      console.log(`---------- DEPOSITO ----------`);
+      console.log(`Voce depositou R$ ${value}`);
+      console.log(`Saldo em conta atualizado: R$ ${this.balance}`);
     }
-  }
+  };
 
-  withdraw = (): void => {
-    console.log('Voce sacou')
-  }
+  withdraw = (value: number): void => {
+    if (this.balance > value) {
+      this.balance -= value;
+      console.log(`---------- SAQUE ----------`);
+      console.log(`Voce sacou R$ ${value}`);
+      console.log(`Saldo em conta atualizado: R$ ${this.balance}`);
+      console.log();
+    } else {
+      console.log(`---------- OPERAÇÃO INVÁLIDA ----------`);
+      console.log(
+        `Saldo insuficiente para saque. Saldo atual em conta: R$ ${this.balance}`
+      );
+      console.log();
+    }
+  };
 
   getBalance = (): void => {
-    console.log(this.balance)
-  }
+    console.log(this.balance);
+  };
 
   private validateStatus = (): boolean => {
     if (this.status) {
-      return this.status
+      return this.status;
     }
 
-    throw new Error('Conta inválida')
-  }
+    throw new Error("Conta inválida");
+  };
 }
