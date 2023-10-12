825abba6f33c948d332ab4573afd092f25f933ed
Matheus Graciki <matheusgraciki7@gmail.com>
2023-10-12 19:27:43 -0300

    feat(ValidateAccount): Implement account validation and refactor the code
    This commit introduces the 'ValidateAccount' feature, enabling account validation and deposit functionality. The code has been refactored for improved readability and maintainability.
    
    - Refactor the 'setName' method .
    - Refactor the 'getName' method.
    - Implemented 'setAccountStatus' to set the account validation status.
    - Introduced 'isAccountValid' for checking the account validation status.

--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -2,40 +2,39 @@ export abstract class DioAccount {
     private name: string;
     private accountNumber: number;
     private balance: number = 100;
-    private accountStatusIsValid: boolean = true;
+    private accountIsValid: boolean = false;
 
     constructor(name: string, accountNumber: number) {
         this.name = name;
         this.accountNumber = accountNumber;
     }
 
-    setName = (name: string): void => {
+    public setName(name: string): void {
         this.name = name;
         console.log('Your name has been altered successfully!');
-    };
+    }
 
-    getName = (): string => {
+    public getName(): string {
         return this.name;
-    };
+    }
 
-    deposit = (amountToDeposit: number): number => {
-        if (this.isValidAccount()) {
+    public setAccountStatus(status: boolean): void {
+        this.accountIsValid = status;
+    }
+
+    public isAccountValid(): boolean {
+        if (this.accountIsValid) {
+            return this.accountIsValid;
+        }
+        throw new Error('Please, validate your account');
+    }
+
+    public deposit(amountToDeposit: number): number {
+        if (this.accountIsValid) {
             this.balance += amountToDeposit;
             return this.balance;
         }
-        throw new Error('You must validate your account before deposit');
-    };
+        throw new Error('Your account is not valid for deposit');
 
-    withdraw = (): void => {};
-
-    getBalance = (): void => {
-        console.log(this.balance);
-    };
-
-    private isValidAccount = (): boolean => {
-        if (this.accountStatusIsValid) {
-            return true;
-        }
-        throw new Error('Your account is not valid');
-    };
+    }
 }
