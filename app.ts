1079de2b0a95d33c4676015a9c59d7d8f517e8f0
Everton Barbosa <104376567+nbeverton@users.noreply.github.com>
2023-11-26 17:10:24 -0300

    Projeto desafio DIO

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
+import { log } from 'console'
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
+import { SpecialAccount } from './class/SpecialAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
 console.log(peopleAccount)
-peopleAccount.deposit()
+peopleAccount.deposit(100)
+
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
+companyAccount.deposit(100)
+companyAccount.getLoan(100)
 console.log(companyAccount)
+
+const specialAccount: SpecialAccount = new SpecialAccount('Everton', 30)
+specialAccount.deposit(100)
+console.log(specialAccount);
--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -6,7 +6,12 @@ export class CompanyAccount extends DioAccount {
     super(name, accountNumber)
   }
 
-  getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
+  getLoan = (amount: number): void => {
+    if (this.getStatus()) {
+      this.balance += amount;
+      console.log('Voce pegou um empréstimo')
+    } else{
+      throw new Error("Não é possível pegar um empréstimo com a conta desativada")
+    }
   }
 }
